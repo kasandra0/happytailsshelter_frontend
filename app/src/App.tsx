33da6a377d449962bfcd0386689759f0c226bb93
kasandra0 <44381697+kasandra0@users.noreply.github.com>
2026-02-15 18:46:01 -0700

    Added ShadCn Select menu component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,13 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 function App() {
   return (
@@ -10,7 +18,20 @@ function App() {
         <Button>default</Button>
         <Button variant="outline">outline</Button>
         <Button variant="secondary">secondary</Button>
-        
+        <br />
+        <br />
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a role" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="fosterparent">Foster Parent</SelectItem>
+              <SelectItem value="volunteer">Volunteer</SelectItem>
+              <SelectItem value="employee">Employee</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
     </>
   );
