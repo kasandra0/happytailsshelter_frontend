c203290462aa12d2a0154909424bb87de3fef79b
kasandra0 <44381697+kasandra0@users.noreply.github.com>
2026-02-15 18:05:01 -0700

    Added ShadCn Button

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <div>
         <h1 className="text-primary text-3xl underline">Hello world!</h1>
+
+        <Button>default</Button>
+        <Button variant="outline">outline</Button>
+        <Button variant="secondary">secondary</Button>
+        
       </div>
     </>
   );
