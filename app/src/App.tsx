7aba3e3d7e8e95e18b83d9d295cdc691b310e792
schraderbachar <jj.schraderbachar@icloud.com>
2026-02-20 19:18:27 -0600

    added animal listing component and cards as well

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,30 @@
 import React from "react";
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "./components/ui/sidebar";
 import { AppSidebar } from "./components/app-sidebar";
 import Demo from "./pages/ComponentDemos";
+import AnimalListingComponent from "./components/animal-listing/animalListingComponent";
 
 function App() {
-
   return (
     <>
       <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 items-center">
-        <h1 className="text-primary text-3xl underline">Hello world!</h1>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4 items-center">
+            <h1 className="text-primary text-3xl underline">Hello world!</h1>
 
-        <Demo />
-      </div>
-      </SidebarInset>
+            <AnimalListingComponent />
+
+            <Demo />
+          </div>
+        </SidebarInset>
       </SidebarProvider>
     </>
   );
---
+++ app/src/types/animal.ts
@@ -0,0 +1,16 @@
+export type Animal = {
+  animal_id: number;
+  microchip: string | null;
+  name: string;
+  date_of_birth: Date | null;
+  gender: string | null;
+  color: string | null;
+  breed: string | null;
+  species: string;
+  weight: number | null;
+  status: string;
+  description: string | null;
+  photo_url: string | null;
+  created_at: Date | null;
+  updated_at: Date | null;
+};
