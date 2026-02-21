94546338f1a879d7a0365082bfd38261f3ea4486
kasandra0 <44381697+kasandra0@users.noreply.github.com>
2026-02-20 17:49:31 -0700

    added React Router for page navigation

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
 import { AppSidebar } from "./components/app-sidebar";
 import Demo from "./pages/ComponentDemos";
+import { LoginForm } from "./pages/LoginPage";
+import { DashboardPage } from "./pages/DashboardPage";
 
 function App() {
 
   return (
     <>
-      <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 items-center">
-        <h1 className="text-primary text-3xl underline">Hello world!</h1>
-
-        <Demo />
-      </div>
-      </SidebarInset>
-      </SidebarProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<></>} /> {/* landing page */}
+        <Route path="/dashboard" element={<DashboardPage />} /> {/* dashboard page */}
+        <Route path="/login" element={<LoginForm />} />
+        
+        <Route path="/components" element={<Demo />} />  {/* page to demo components */}
+        <Route path="/*" element={<></>} /> {/* catch all route for 404 page */}
+        
+        {/* Add more routes here as needed */}
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
