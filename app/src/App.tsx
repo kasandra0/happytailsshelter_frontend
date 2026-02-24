06d881702bc34270d13410083e4fa9ee54d8d788
Claudia Dominguez <126995375+CV17-09@users.noreply.github.com>
2026-02-24 10:25:51 -0600

    changed

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
             <Route path="/animals/:id" element={<AnimalProfilePage animal={{} as any} />} />
             <Route path="/animals/:id/adopt" element={<AdoptPage />} />
 
-            <Route path="/inventory" element={<Demo />} />
+            <Route path="/inventory" element={<Demo />} />ls
 
             <Route path="/components" element={<Demo />} />
 
@@ -35,26 +35,19 @@ function App() {
       <div className="flex flex-1 flex-row p-4">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<></>} /> {/* landing page */}
+            <Route path="/" element={<></>} /> 
             <Route path="/staff/dashboard" element={<DashboardPage />} />
-            {/* dashboard page */}
             <Route path="/login" element={<LoginForm />} />
             <Route
               path="/staff/animals"
               element={<AnimalListingPage />}
             />
-            {/* list of all animals in the shelter */}
             <Route path="/staff/animals/:id" element={<Demo />} />
-            {/* individual animal profile page */}
             <Route path="/staff/animals/new" element={<Demo />} />
-            {/* intake form to add a new animal */}
             <Route path="/staff/inventory" element={<Demo />} />
-            {/* list of all inventory items in shelter */}
             <Route path="/components" element={<Demo />} />
-            {/* page to demo components */}
             <Route path="/*" element={<Error404 />} />
-            {/* catch all route for 404 page */}
-            {/* Add more routes here as needed */}
+
           </Routes>
         </BrowserRouter>
       </div>
